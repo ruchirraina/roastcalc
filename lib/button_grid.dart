5a2c70a9024926b2a36df7a6c871c8281c251d0c
Ruchir <ruchirraina@gmail.com>
2026-01-28 17:12:26 +0530

    History panel sliding  implement + Dynamic animated grid layout

--- lib/button_grid.dart
+++ lib/button_grid.dart
@@ -5,9 +5,19 @@ class ButtonGrid extends StatelessWidget {
   // box parameters that hold circular buttons with padding
   final double boxWidth;
   final double boxHeight;
+  final double boxHeightShrink;
+
+  // boolean value for history panel open
+  final bool panelOpen;
 
   // recieving box parameters
-  ButtonGrid({required this.boxWidth, required this.boxHeight, super.key});
+  ButtonGrid({
+    required this.boxWidth,
+    required this.boxHeight,
+    required this.boxHeightShrink,
+    required this.panelOpen,
+    super.key,
+  });
 
   // all button labels
   final List<String> _buttonLabels = [
@@ -37,109 +47,58 @@ class ButtonGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        // movable backspace button when history panel slides in
-        Positioned(
-          top: 0,
-          left: boxWidth,
-          child: SizedBox(
-            height: boxHeight,
-            width: boxWidth,
-            child: Padding(
-              padding: .all(4),
-              child: FilledButton.tonal(
-                style: FilledButton.styleFrom(shape: const CircleBorder()),
-                onPressed: () {},
-                child: Icon(
-                  Icons.backspace_outlined,
-                  size: 28,
-                  applyTextScaling: true,
-                ),
-              ),
-            ),
-          ),
-        ),
-
-        // shrinking operator buttons that shrink when history panel slides in
-        for (int i = 0; i < 5; i++)
-          Positioned(
-            top: i * boxHeight,
-            right: 0,
-            child: SizedBox(
-              height: boxHeight,
-              width: boxWidth,
-              child: Padding(
-                padding: .all(4),
-                child: (i == 4)
-                    // to emphasize equal to button
-                    ? FilledButton(
-                        style: FilledButton.styleFrom(
-                          shape: const CircleBorder(),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          _buttonLabels[19],
-                          style: context.textTheme.displaySmall!.copyWith(
-                            color: context.colorScheme.onPrimary,
-                            fontWeight: .bold,
-                          ),
-                        ),
-                      )
-                    // other operator buttons
-                    : FilledButton.tonal(
-                        style: FilledButton.styleFrom(
-                          shape: const CircleBorder(),
-                        ),
-                        onPressed: () {},
-                        child: Text(
-                          _buttonLabels[4 * (i + 1) - 1],
-                          style: context.textTheme.displaySmall!.copyWith(
-                            fontWeight: .bold,
-                          ),
-                        ),
-                      ),
-              ),
-            ),
-          ),
-
         // static buttons that can fade out when history panel slides in
-        for (int i = 0; i < 5; i++)
-          for (int j = 0; j < 3; j++)
+        // loop based generation and posiitoning of buttons
+        for (int i = 0; i < 5; i++) // rows
+          for (int j = 0; j < 3; j++) // columns
+            // skipping 1st row and 2nd column (occupied by movable button)
             if (i != 0 || j != 1)
               Positioned(
                 top: i * boxHeight,
                 left: j * boxWidth,
-                child: SizedBox(
-                  height: boxHeight,
-                  width: boxWidth,
+                height: boxHeight,
+                width: boxWidth,
+                child: AnimatedOpacity(
+                  // fade before history panel slides in
+                  opacity: panelOpen ? 0 : 1,
+                  // fade quick before history panel slides in
+                  duration: const Duration(milliseconds: 100),
+                  // linear curve simple
+                  curve: Curves.linear,
                   child: Padding(
+                    // padding around filled button
                     padding: .all(4),
                     // AC and % button
                     child: (i == 0 && (j == 0 || j == 2))
                         ? FilledButton.tonal(
                             style: FilledButton.styleFrom(
+                              // circular
                               shape: const CircleBorder(),
                             ),
-                            onPressed: () {},
+                            onPressed: () {}, // non functional
                             child: Text(
                               _buttonLabels[j],
                               style:
-                                  (j == 0) // for AC button
+                                  (j == 0) // appropriate style for AC button
                                   ? context.textTheme.headlineSmall!.copyWith(
                                       fontWeight: .bold,
                                     )
-                                  // for % button
+                                  // appropriate style for % button
                                   : context.textTheme.displaySmall!.copyWith(
                                       fontWeight: .bold,
                                     ),
                             ),
                           )
+                        // numbers
                         : ElevatedButton(
                             style: ElevatedButton.styleFrom(
+                              // circular
                               shape: const CircleBorder(),
                             ),
-                            onPressed: () {},
+                            onPressed: () {}, // non functional
                             child: Text(
                               _buttonLabels[4 * i + j],
+                              // appropriate style for numbers
                               style: context.textTheme.headlineLarge!.copyWith(
                                 color: context.colorScheme.primary,
                                 fontWeight: .bold,
@@ -149,6 +108,102 @@ class ButtonGrid extends StatelessWidget {
                   ),
                 ),
               ),
+
+        // operator buttons that shrink when history panel slides in
+        Positioned(
+          right: 0, // positioned on right
+          child: Column(
+            children: [
+              // a dynamaic box to move the other buttons down when they shrink
+              AnimatedContainer(
+                // appropriate duration
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.fastOutSlowIn, // appropriate curve
+                // dynamic height - 0 when panel not present
+                height: panelOpen ? boxHeightShrink : 0,
+                width: boxWidth,
+              ),
+              // operator buttons
+              // loop based generation and position of buttons
+              for (int i = 0; i < 5; i++) // column
+                AnimatedContainer(
+                  // appropriate duration
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.fastOutSlowIn, // appropriate curve
+                  // dynamic height
+                  height: panelOpen ? boxHeightShrink : boxHeight,
+                  width: boxWidth,
+                  child: Padding(
+                    // padding around filled button
+                    padding: .all(4),
+                    child: (i == 4)
+                        // filled to emphasize equal to button
+                        ? FilledButton(
+                            // circular
+                            style: FilledButton.styleFrom(
+                              shape: const CircleBorder(),
+                            ),
+                            onPressed: () {}, // non functional rn
+                            child: Text(
+                              _buttonLabels[19],
+                              // appropriate size and style for =
+                              style: context.textTheme.displaySmall!.copyWith(
+                                color: context.colorScheme.onPrimary,
+                                fontWeight: .bold,
+                              ),
+                            ),
+                          )
+                        // other operator buttons as tonal filled
+                        : FilledButton.tonal(
+                            // circular
+                            style: FilledButton.styleFrom(
+                              shape: const CircleBorder(),
+                            ),
+                            onPressed: () {}, // non functional rn
+                            child: Text(
+                              _buttonLabels[4 * (i + 1) - 1],
+                              // appropriate size and style for operators
+                              style: context.textTheme.displaySmall!.copyWith(
+                                fontWeight: .bold,
+                              ),
+                            ),
+                          ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+
+        // movable backspace button when history panel slides in
+        AnimatedPositioned(
+          top: 0, // in first row
+          // first in 2nd column then slides to 4th column
+          left: panelOpen ? 3 * boxWidth : boxWidth,
+          duration: const Duration(milliseconds: 300), // appropriate duration
+          curve: Curves.fastOutSlowIn, // appropriate curve
+          // dynamic size - shrink to fit into 4th column
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 300), // appropriate duration
+            curve: Curves.fastOutSlowIn, // appropriate curve
+            // shrink height
+            height: panelOpen ? boxHeightShrink : boxHeight,
+            width: boxWidth,
+            child: Padding(
+              // padding around filled button
+              padding: .all(4),
+              child: FilledButton.tonal(
+                // circular
+                style: FilledButton.styleFrom(shape: const CircleBorder()),
+                onPressed: () {}, // non functional rn
+                child: Icon(
+                  Icons.backspace_outlined,
+                  size: 28, // appropriate size
+                  applyTextScaling: true, // so it doesn't look tiny if font big
+                ),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
--- lib/home.dart
+++ lib/home.dart
@@ -2,43 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:roastcalc/button_grid.dart';
 import 'package:roastcalc/theme_extension.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  // controls if history slide panel visibile or not and grid layout
+  bool _panelOpen = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // history Button
-        // TODO: implement slide panel that shows history of applications
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.history)),
+        // history Button - rn trigger grid animation and slides panel but empty
+        // TODO: implement scrollable list that shows history of calculations
+        leading: IconButton(
+          onPressed: () {
+            setState(() {
+              _panelOpen = !_panelOpen;
+            });
+          },
+          icon: Icon(Icons.history),
+        ),
         // info about app and me
         // TODO: implement popup that displays app version and my tech socials
         actions: [IconButton(onPressed: () {}, icon: Icon(Icons.info_outline))],
       ),
 
-      body: Column(
-        children: [
-          // TODO: implement - display + roast
-          Expanded(
-            flex: 3,
-            child: Container(color: context.colorScheme.secondary),
-          ),
-          //TODO: implement - calculator grid (first visual then working)
-          Expanded(
-            flex: 5,
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                // box width that holds circular button with padding
-                final double boxWidth = constraints.maxWidth / 4;
-                // box height that holds circular button with padding
-                final double boxHeight = constraints.maxHeight / 5;
-
-                return ButtonGrid(boxWidth: boxWidth, boxHeight: boxHeight);
-              },
+      // nav buttons in some androids overlay so safe area
+      body: SafeArea(
+        child: Column(
+          children: [
+            // TODO: implement - display + roast
+            Expanded(
+              flex: 3,
+              child: Container(color: context.colorScheme.secondary),
             ),
-          ),
-        ],
+            //TODO: implement - calculator grid - rn animated but non functional
+            Expanded(
+              flex: 5,
+              child: LayoutBuilder(
+                builder: (context, constraints) {
+                  // box height that holds circular button with padding
+                  final double boxHeight = constraints.maxHeight / 5;
+                  // box height shrink that holds circular button with padding
+                  final double boxHeightShrink = constraints.maxHeight / 6;
+                  // box width that holds circular button with padding
+                  final double boxWidth = constraints.maxWidth / 4;
+
+                  // stack with history panel on top and button grid at bottom
+                  return Stack(
+                    children: [
+                      // bottom
+                      ButtonGrid(
+                        boxWidth: boxWidth,
+                        boxHeight: boxHeight,
+                        boxHeightShrink: boxHeightShrink,
+                        panelOpen: _panelOpen,
+                      ),
+                      // top
+                      AnimatedPositioned(
+                        // left of screen boundaries orignally
+                        left: _panelOpen ? 0 : -(boxWidth * 3),
+                        height: constraints.maxHeight,
+                        width: 3 * boxWidth,
+                        // appropriate duration
+                        duration: const Duration(milliseconds: 300),
+                        // approrpriate curve
+                        curve: Curves.fastOutSlowIn,
+                        child: Card(),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
